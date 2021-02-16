591b414ef59af120b38349d999b6d80ad4c201e6
mohanpa101 <30289789+mohanpa101@users.noreply.github.com>
2021-02-16 16:14:23 +0530

    Create script.sh
    initial commit

---
+++ script.sh
@@ -0,0 +1,9 @@
+##SampleLogLines
+#192.168.2.20 - - [28/Jul/2006:10:27:10 -0300] "GET /cgi-bin/try/ HTTP/1.0" 404 3395
+#127.0.0.1 - - [28/Jul/2006:10:22:04 -0300] "GET / HTTP/1.0" 401 2216
+
+
+#Getting malicous_ips from apache access log (Default Log Patter) check sample file 
+#!/bin/bash 
+
+cat access.log | grep 401"\|"404 | awk '{print $1}' | sort | uniq -c | awk '{if ($1 > 1)print $2}' > malicous_ips 
