252b71c5cf5be56b5720f1613ff37e0d7516c428
mohanpa101 <30289789+mohanpa101@users.noreply.github.com>
2021-02-16 16:27:35 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,15 @@
 #127.0.0.1 - - [28/Jul/2006:10:22:04 -0300] "GET / HTTP/1.0" 401 2216
 
 #Getting malicous_ips from apache access log (Default Log Pattern) check sample logs above
-cat access.log | grep 401"\|"404 | awk '{print $1}' | sort | uniq -c | awk '{if ($1 > 100)print $2}' > malicous_ips 
+cat access.log | grep 401"\|"404 | awk '{print $1}' | sort | uniq -c | awk '{if ($1 > 100)print $2}' >> malicous_ips
+
+while IFS="" read -r p || [ -n "$p" ]
+do
+   echp "Require not ip $p" >> malicous_ip.conf
+done < malicous_ip
+
+
+#Add  malicous_ips conf file path to apache config as 'Include malicous_ip.conf'
+#restart apache
+systemctl restart httpd
 
